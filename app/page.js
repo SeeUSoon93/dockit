5443502718dafdade2616dd47a9bc098ca2f59fe
순이 <rlarnstns@gmail.com>
2025-09-11 17:25:16 +0900

    Tailwind 삭제

--- app/page.js
+++ app/page.js
@@ -1,7 +1,15 @@
+"use client";
+import { LogoGoogle } from "sud-icons";
+import { Button, Typography } from "sud-ui";
+
 export default function Home() {
   return (
-    <>
-      <h1>Welcome to DocKit</h1>
-    </>
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      {/* 메인페이지는 나중에 꾸미기 */}
+      {/* 우선은 로그인만 구현 */}
+      <Button icon={<LogoGoogle />}>
+        <Typography>Google로 시작하기</Typography>
+      </Button>
+    </div>
   );
 }
