75c0937e034118d9c7b296f1db3f6da2b5af9d48
순이 <rlarnstns@gmail.com>
2025-09-11 14:28:03 +0900

    dockit

--- app/LIB/utils/dataUtils.js
+++ app/LIB/utils/dataUtils.js
@@ -1,12 +1,12 @@
 import { API_BASE_URL } from "../config/config";
 import { callApi } from "./apiUtils";
 
-// 문서 생성
+// CREATE
 export const createData = async (contentType) => {
   return callApi(`${API_BASE_URL}/${contentType}/create`, { method: "POST" });
 };
 
-// 문서 저장
+// UPDATE
 export const updateData = async (contentType, docId, doc) => {
   return callApi(`${API_BASE_URL}/${contentType}/update/${docId}`, {
     method: "PUT",
@@ -14,19 +14,19 @@ export const updateData = async (contentType, docId, doc) => {
   });
 };
 
-// id로 문서 불러오기
+// READ ONE by ID
 export const fetchData = async (contentType, docId) => {
   return callApi(`${API_BASE_URL}/${contentType}/get/${docId}`, {
     method: "GET"
   });
 };
 
-// 전체 문서 불러오기
+// READ ALL
 export const fetchDataList = async (contentType) => {
   return callApi(`${API_BASE_URL}/${contentType}`, { method: "GET" });
 };
 
-// id로 문서 삭제
+// DELETE by ID
 export const deleteData = async (contentType, docId) => {
   return callApi(`${API_BASE_URL}/${contentType}/delete/${docId}`, {
     method: "DELETE"
