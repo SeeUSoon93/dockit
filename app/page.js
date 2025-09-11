a5b24677e8942cdc58b75a44d0ffd568d7eb0371
순이 <rlarnstns@gmail.com>
2025-09-11 18:02:31 +0900

    페이지 설정 닷

--- app/page.js
+++ app/page.js
@@ -3,19 +3,26 @@ import { LogoGoogle } from "sud-icons";
 import { Button, Typography } from "sud-ui";
 import { handleGoogleAuth } from "./LIB/utils/authUtils";
 import { useUser } from "./LIB/context/UserContext";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
   const { user } = useUser();
-  console.log(user);
+  const router = useRouter();
+
+  const onLogin = async () => {
+    try {
+      await handleGoogleAuth();
+      router.push("/workspace");
+    } catch (error) {
+      console.error("로그인 중 오류 발생:", error);
+    }
+  };
+
   return (
-    <div className="flex flex-col items-center justify-center w-full min-h-screen">
+    <div className="flex flex-col items-center justify-center">
       {/* 메인페이지는 나중에 꾸미기 */}
       {/* 우선은 로그인만 구현 */}
-      <Button
-        icon={<LogoGoogle />}
-        shape="capsule"
-        onClick={async () => await handleGoogleAuth()}
-      >
+      <Button icon={<LogoGoogle />} shape="capsule" onClick={onLogin}>
         <Typography className="px-20 py-10">Google로 시작하기</Typography>
       </Button>
     </div>
